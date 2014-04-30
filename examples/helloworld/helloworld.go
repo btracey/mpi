36c36df9a3fae49a304862bc3bf04cf8b2162688
btracey <bdtinternet@gmail.com>
2014-04-30 01:48:59 -0700

    uncommented

--- examples/helloworld/helloworld.go
+++ examples/helloworld/helloworld.go
@@ -19,6 +19,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/btracey/mpi"
 )
@@ -41,35 +42,34 @@ func main() {
 	size := mpi.Size()
 	fmt.Printf("Hello world, my rank is %v out of %v\n", rank, size)
 
-	/*
-		// Send and receive messages to and from everyone concurrently
-		wg := &sync.WaitGroup{}
-		wg.Add(size)
-		for i := 0; i < mpi.Size(); i++ {
-			go func(i int) {
-				defer wg.Done()
-				str := fmt.Sprintf("\"Hello node %v, I'm node %v\"", i, rank)
-				if i == rank {
-					str = "\"I'm just talking to myself\""
-				}
-				err := mpi.Send(str, i, 0)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}(i)
-		}
-		wg.Add(size)
-		for i := 0; i < mpi.Size(); i++ {
-			go func(i int) {
-				defer wg.Done()
-				var str string
-				err := mpi.Receive(&str, i, 0)
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Printf("I, node %v, received a message: %v\n", rank, str)
-			}(i)
-		}
-		wg.Wait()
-	*/
+	// Send and receive messages to and from everyone concurrently
+	wg := &sync.WaitGroup{}
+	wg.Add(size)
+	for i := 0; i < mpi.Size(); i++ {
+		go func(i int) {
+			defer wg.Done()
+			str := fmt.Sprintf("\"Hello node %v, I'm node %v\"", i, rank)
+			if i == rank {
+				str = "\"I'm just talking to myself\""
+			}
+			err := mpi.Send(str, i, 0)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(i)
+	}
+	wg.Add(size)
+	for i := 0; i < mpi.Size(); i++ {
+		go func(i int) {
+			defer wg.Done()
+			var str string
+			err := mpi.Receive(&str, i, 0)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("I, node %v, received a message: %v\n", rank, str)
+		}(i)
+	}
+	wg.Wait()
+
 }
